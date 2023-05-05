b46168a6e3ee868348d787e6837e9f8a21438ff3
Matthew Stoodley <matt.storyfeet@gmail.com>
2023-05-05 10:25:57 +0100

    now outputs json using clap in main program

--- src/card_check.rs
+++ src/card_check.rs
@@ -1,16 +1,39 @@
 extern crate card_format;
 use std::io::{self, Read};
+use clap::{Arg,arg,Command,crate_version};
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let matches = Command::new("card_check")
+        .version(crate_version!())
+        .about("A program to check and convert card_format from stdin to stdout")
+        .author("Matthew Stoodley (storyfeet)")
+        .subcommand(Command::new("json"))
+        .get_matches();
+  
+
     let mut buf = String::new();
     let mut sin = io::stdin();
     sin.read_to_string(&mut buf)?;
 
     let ar = card_format::parse_cards(&buf)?;
 
-    for (i, c) in ar.iter().enumerate() {
-        println!("{} = {}", i, c);
+    match matches.subcommand() {
+        Some(("json",_)) => {
+            print!("{}", serde_json::to_string_pretty(&ar)?);
+        }
+
+        _ => for (i, c) in ar.iter().enumerate() {
+            println!("{} = {}", i, c);
+        }
     }
+
+
+
+    
 
     Ok(())
 }
+
+
+
